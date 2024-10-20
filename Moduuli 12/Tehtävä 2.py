e452c1b75e9359d49710d80460f127c66c203681
Grokeheart <46874717+Grokeheart@users.noreply.github.com>
2024-10-20 13:00:04 +0300

    Update Tehtävä 2.py

--- Moduuli 12/Tehtävä 2.py
+++ Moduuli 12/Tehtävä 2.py
@@ -2,7 +2,7 @@ import requests
 
 paikkakunta = input("Syötä paikkakunta: ")
 
-api_key = "5ee562d3cae1a968b23b545d0959a16c"
+api_key = ""
 
 res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={paikkakunta}&limit=1&appid={api_key}").json()
 
